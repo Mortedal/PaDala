4756ba808808d1e9b921c292889cca7d2d654ed7
Mortedal <112877163+Mortedal@users.noreply.github.com>
2022-09-05 23:50:01 +0800

    init

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import BannerPic from '../assets/bannerPic.png'
+import '../styles/Home.css'
+
+function Home() {
+  return (
+    <div className='home'>
+        <div
+        className='headerContainer'
+        style={{ backgroundImage: `url(${BannerPic})` }}>
+        
+            <h1>Pa-dala</h1>
+            <p>Erranding Service</p>
+            <Link to ="/login">
+            <button>Log-in</button>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Home
