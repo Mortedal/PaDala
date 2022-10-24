5abbc6226a4de093b701a19f638cccbdca472488
Mortedal <112877163+Mortedal@users.noreply.github.com>
2022-10-24 23:58:35 +0800

    FIX

--- client/src/App.js
+++ client/src/App.js
@@ -15,52 +15,6 @@ import AllTransactions from "./pages/AllTransactions";
 import CustomerProfile from "./pages/CustomerProfile";
 import RiderProfile from "./pages/RiderProfile";
 import PopupForm from "./components/PopupForm";
-
-<<<<<<< HEAD
-import './App.css';
-import Authen from  './components/Authen';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Services from './pages/Services';
-import About from './pages/About';  
-import Dashboard from './pages/Dashboard';  
-import Chat from './pages/Chat';  
-import AdminDashboard from './pages/AdminDash';  
-import ProfileDash from './pages/ProfileDash';
-import TransactionsDash from './pages/TransactionsDash';
-import AddressDash from './pages/AddressDash';
-import EditProfile from './pages/EditDash';
-import PopupForm from './components/PopupForm';
-
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-function App() {
-  return<div className="App"> 
-    <Router>
-    <Navbar />
-    <Routes>
-      <Route element={<Authen/>}>
-      <Route path='/dashboard' exact element={<Dashboard/>}/>
-      <Route path='/admineditprofile' exact element={<AdminDashboard/>}/>
-      <Route path='/profile' exact element={<ProfileDash/>}/>
-      <Route path='/transaction' exact element={<TransactionsDash/>}/>
-      <Route path='/address' exact element={<AddressDash/>}/>
-      <Route path='/editprofile' exact element={<EditProfile/>}/>
-      </Route>
-      <Route path='/' exact element={<Home/>}/>
-      <Route path='/services' exact element={<Services/>}/>
-      <Route path='/about' exact element={<About/>}/>
-      <Route path='/chat' exact element={<Chat/>}/>
-      <Route path='/signup' exact element={<Signup/>}/>
-      <Route path='/login' exact element={<Login/>}/>
-      <Route path='/popupform' exact element={<PopupForm/>}/>
-      </Routes>
-    <Footer />
-    </Router>
-=======
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -99,7 +53,6 @@ function App() {
         </Routes>
         <Footer />
       </Router>
->>>>>>> fc47e1e9e6a48530324dea372012893b8e5a34c8
 
       {/*-------------------------------for design-------------------------------------------------*/}
 
--- client/src/components/SidebarDataAdmin.js
+++ client/src/components/SidebarDataAdmin.js
@@ -1,35 +1,4 @@
 export const SidebarDataAdmin = [
-<<<<<<< HEAD
-    {
-        title:"Profile",
-        link: "/profile",
-    },
-    {
-        title:"Transaction",
-        link: "/transaction",
-
-    },
-    {
-        title:"Customer Profiles",
-        link: "/profile",
-    },
-    {
-        title:"Rider Profiles",
-        link: "/transaction",
-
-    },
-    {
-        title:"Edit User Roles",
-        link: "/address",
-
-    },
-    {
-        title:"Delete Users",
-        link: "/address",
-
-    },
-]
-=======
   {
     title: "Profile",
     link: "/profile",
@@ -51,4 +20,3 @@ export const SidebarDataAdmin = [
     link: "/riderprofile",
   },
 ];
->>>>>>> fc47e1e9e6a48530324dea372012893b8e5a34c8
