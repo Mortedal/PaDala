6c832b8fcd8b9ff95573e0d625ab32f5f3935efc
Mortedal <112877163+Mortedal@users.noreply.github.com>
2022-10-26 01:27:21 +0800

    FIX POTA

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,9 +1,10 @@
 import React from "react";
-import MessengerCustomerChat from "react-messenger-customer-chat";
+// import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function Chat() {
   return (
-    <MessengerCustomerChat pageId="100086780636440" appId="521750459399393" />
+    <div></div>
+    // <MessengerCustomerChat pageId="100086780636440" appId="521750459399393" />
   );
 }
 
