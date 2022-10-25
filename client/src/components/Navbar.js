8133f6c03564276643c3ed4122da35ebf36d08aa
Mortedal <112877163+Mortedal@users.noreply.github.com>
2022-10-25 21:03:49 +0800

    try messenger

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,125 +1,141 @@
-import React, { useState } from 'react'
-import Logo from '../assets/logopd.png'
-import {Link, useNavigate} from "react-router-dom";
-import ReorderIcon from '@mui/icons-material/Reorder';
-import '../styles/Navbar.css';
+import React, { useState } from "react";
+import Logo from "../assets/logopd.png";
+import { Link, useNavigate } from "react-router-dom";
+import ReorderIcon from "@mui/icons-material/Reorder";
+import "../styles/Navbar.css";
 
 function Navbar() {
-
   const navigate = useNavigate();
 
-const [openLinks, setOpenLinks] = useState(false)
+  const [openLinks, setOpenLinks] = useState(false);
 
-const auth = localStorage.getItem('user');
+  const auth = localStorage.getItem("user");
 
-const toggleNavbar = () => {
+  const toggleNavbar = () => {
     setOpenLinks(!openLinks);
-}
+  };
 
-const logout = () => {
-  localStorage.clear();
-  navigate('/')
-}
+  const logout = () => {
+    localStorage.clear();
+    navigate("/");
+  };
 
-    
   return (
     //--------------------------------------------------With Authentication-------------------------------------------
-    
-    <div className='navbar'>
-      {
-          auth ?
-        <div className='leftSide' id={openLinks ? "open" : "close"}> 
-         <img src={Logo} alt="logo"/>
-         <div className='hiddenLinks'>
-          
-         <Link to ="/"> Home </Link>
-         <Link to ="/services"> Services </Link>
-         <Link to ="/about"> About  </Link>
-         <Link to ="/dashboard"> Dashboard  </Link>
-         <Link to ="/chat"> Chat </Link>
 
-         {auth ? <Link onClick={logout} to ="/"> Logout </Link> : <Link to ="/login"> Login  </Link>}
-         
-         
-         </div>
+    <div className="navbar">
+      {auth ? (
+        <div className="leftSide" id={openLinks ? "open" : "close"}>
+          <img src={Logo} alt="logo" />
+          <div className="hiddenLinks">
+            <Link to="/"> Home </Link>
+            <Link to="/services"> Services </Link>
+            <Link to="/about"> About </Link>
+            <Link to="/dashboard"> Dashboard </Link>
+            <Link to="/chat"> Chat </Link>
+
+            {auth ? (
+              <Link onClick={logout} to="/">
+                {" "}
+                Logout{" "}
+              </Link>
+            ) : (
+              <Link to="/login"> Login </Link>
+            )}
+          </div>
         </div>
-        :
+      ) : (
+        <div className="leftSide" id={openLinks ? "open" : "close"}>
+          <img src={Logo} alt="logo" />
+          <div className="hiddenLinks">
+            <Link to="/"> Home </Link>
+            <Link to="/services"> Services </Link>
+            <Link to="/about"> About </Link>
 
-        <div className='leftSide' id={openLinks ? "open" : "close"}> 
-         <img src={Logo} alt="logo"/>
-         <div className='hiddenLinks'>
-          
-         <Link to ="/"> Home </Link>
-         <Link to ="/services"> Services </Link>
-         <Link to ="/about"> About  </Link>
-         
-
-         {auth ? <Link onClick={logout} to ="/"> Logout </Link>:<Link to ="/login"> Login  </Link>}
-         
-         </div>
+            {auth ? (
+              <Link onClick={logout} to="/">
+                {" "}
+                Logout{" "}
+              </Link>
+            ) : (
+              <Link to="/login"> Login </Link>
+            )}
+          </div>
         </div>
-      }
+      )}
 
-        {
-          auth ?
-            <div className='rightSide'>
-              <Link to ="/"> Home </Link>
-              <Link to ="/services"> Services </Link>
-              <Link to ="/about"> About  </Link>
-              <Link to ="https://www.messenger.com/"> Chat </Link>
-              {
-               JSON.parse(auth).role === "" ? <Link to ="/profile"> Dashboard  </Link>: ''
-              }  
-              {
-               JSON.parse(auth).role === "admin" ? <Link to ="/profile"> Admin Dashboard  </Link>: ''
-              }  
-              {
-               JSON.parse(auth).role === "rider" ? <Link to ="/profile"> Rider Dashboard  </Link>: ''
-              }  
-              {auth ? <Link onClick={logout} to ="/"> Logout </Link>:<Link to ="/login"> Login  </Link>}
+      {auth ? (
+        <div className="rightSide">
+          <Link to="/"> Home </Link>
+          <Link to="/services"> Services </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/chat"> Chat </Link>
+          {JSON.parse(auth).role === "" ? (
+            <Link to="/profile"> Dashboard </Link>
+          ) : (
+            ""
+          )}
+          {JSON.parse(auth).role === "admin" ? (
+            <Link to="/profile"> Admin Dashboard </Link>
+          ) : (
+            ""
+          )}
+          {JSON.parse(auth).role === "rider" ? (
+            <Link to="/profile"> Rider Dashboard </Link>
+          ) : (
+            ""
+          )}
+          {auth ? (
+            <Link onClick={logout} to="/">
+              {" "}
+              Logout{" "}
+            </Link>
+          ) : (
+            <Link to="/login"> Login </Link>
+          )}
 
-              <button onClick={toggleNavbar}>
-                  <ReorderIcon />
-              </button>
+          <button onClick={toggleNavbar}>
+            <ReorderIcon />
+          </button>
         </div>
-        
-        :
-        
-        <div className='rightSide'>
-              <Link to ="/"> Home </Link>
-              <Link to ="/services"> Services </Link>
-              <Link to ="/about"> About  </Link>
-              {auth ? <Link onClick={logout} to ="/"> Logout </Link>:<Link to ="/login"> Login  </Link>}
-              
-              <button onClick={toggleNavbar}>
-                  <ReorderIcon />
-              </button>
-        </div>
+      ) : (
+        <div className="rightSide">
+          <Link to="/"> Home </Link>
+          <Link to="/services"> Services </Link>
+          <Link to="/about"> About </Link>
+          {auth ? (
+            <Link onClick={logout} to="/">
+              {" "}
+              Logout{" "}
+            </Link>
+          ) : (
+            <Link to="/login"> Login </Link>
+          )}
 
-        }
-        
-        
+          <button onClick={toggleNavbar}>
+            <ReorderIcon />
+          </button>
+        </div>
+      )}
     </div>
 
     //--------------------------------------------------UI DESIGN-------------------------------------------
 
     // <div className='navbar'>
-      
-          
-    //      <div className='leftSide' id={openLinks ? "open" : "close"}> 
+
+    //      <div className='leftSide' id={openLinks ? "open" : "close"}>
     //      <img src={Logo} alt="logo"/>
     //      <div className='hiddenLinks'>
-          
+
     //      <Link to ="/"> Home </Link>
     //      <Link to ="/services"> Services </Link>
     //      <Link to ="/about"> About  </Link>
     //      <Link to ="/dashboard"> Dashboard  </Link>
 
     //      <Link to ="/login"> Login  </Link>
-         
+
     //      </div>
     //     </div>
-         
 
     //         <div className='rightSide'>
     //           <Link to ="/"> Home </Link>
@@ -127,15 +143,14 @@ const logout = () => {
     //           <Link to ="/about"> About  </Link>
     //           <Link to ="/profile"> Dashboard  </Link>
     //           <Link to ="/login"> Login  </Link>
-              
+
     //           <button onClick={toggleNavbar}>
     //               <ReorderIcon />
     //           </button>
     //     </div>
-        
+
     //     </div>
-      
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,17 +1,10 @@
-import React from 'react'
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+import React from "react";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function Chat() {
-
   return (
-    <div>
-    <MessengerCustomerChat
-    pageId="100086780636440"
-    appId="521750459399393"
-  />,
-
-    </div>
-  )
+    <MessengerCustomerChat pageId="100086780636440" appId="521750459399393" />
+  );
 }
 
-export default Chat
+export default Chat;
