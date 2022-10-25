31957c98f296e3e4e27ec6517213ae3d28e2201a
Mortedal <112877163+Mortedal@users.noreply.github.com>
2022-10-25 22:52:42 +0800

    fix packages

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,40 +1,38 @@
 import React from "react";
 import pdlogo from "../assets/rider.jpg";
 import "../styles/About.css";
-import { Parallax } from 'react-parallax';
+// import Parallax from "react-parallax";
 
 function About() {
   return (
-  <div className="About"> 
-<Parallax strength={600} bgImage={pdlogo}>
-  <div className="content">
-  <div className="text-content">About Us</div>
-  <div className="paragraph-content"></div>
-  </div>
-</Parallax>
-    <p className="paragraph-content">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
-          accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
-          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
-          officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
-          nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
-          error repudiandae fuga? Ipsa laudantium molestias eos sapiente
-          officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
-          cumque velit
-    </p>
+    <div className="About">
+      {/* <Parallax strength={600} bgImage={pdlogo}>
+        <div className="content">
+          <div className="text-content">About Us</div>
+          <div className="paragraph-content"></div>
+        </div>
+      </Parallax> */}
+      <p className="paragraph-content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+        mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+        voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+        fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+        accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+        molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+        officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
+        nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+        error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis
+        modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias
+        error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt
+        dolorem! Officiis iure rerum voluptates a cumque velit
+      </p>
 
-    <Parallax strength={600} bgImage={pdlogo}>
-  <div className="content">
-  <div className="text-content">About Us</div>
-  <div className="paragraph-content"></div>
-  </div>
-
-</Parallax>
+      {/* <Parallax strength={600} bgImage={pdlogo}>
+        <div className="content">
+          <div className="text-content">About Us</div>
+          <div className="paragraph-content"></div>
+        </div>
+      </Parallax> */}
     </div>
   );
 }
