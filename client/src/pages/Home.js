82231d4e43275f102c56dac76155e7e9e5ebda49
Mortedal <112877163+Mortedal@users.noreply.github.com>
2022-10-26 11:37:36 +0800

    dot env

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,13 +5,17 @@ import Button from '@mui/material/Button';
 // import { Parallax } from 'react-parallax';
 import '../styles/Home.css'
 
+const currDate = new Date().toLocaleDateString();
+const currTime = new Date().toLocaleTimeString();
+
 function Home() {
   return (
     <div className='home'>
         <div
         className='headerContainer'
         style={{ backgroundImage: `url(${BannerPic})` }}>
-        
+        <p> Current Date is = {currDate} </p>
+        <p> Curremt Time is = {currTime} </p> 
             <h1>Pa-Dala</h1><br/>
             <p>Erranding Service</p>
             <Link to ="/login" style={{ textDecoration: 'none' }}>
